f8d3c0d4968c48c24ec681bad63bbb1a062ddcfc
Andrea Jemmett <andreajemmett@gmail.com>
2013-12-21 17:34:19 +0100

    Added mockup footer

--- wp-content/themes/coompany-theme/footer.php
+++ wp-content/themes/coompany-theme/footer.php
@@ -8,7 +8,32 @@
  */
 ?>
 
-			<footer>
+			<footer class="row">
+
+				<div class="col-xs-2 col-xs-offset-1">
+					<h6>Prodotti:</h6>
+					<ul class="list-unstyled">
+						<li><a href="#">Coo2plan</a></li>
+					</ul>
+				</div>
+				<div class="col-xs-2">
+					<h6>About:</h6>
+					<ul class="list-unstyled">
+						<li><a href="#">Chi siamo</a></li>
+						<li><a href="#">Contatti</a></li>
+						<li><a href="#">Team</a></li>
+						<li><a href="#">Work with us</a></li>
+					</ul>
+				</div>
+				<div class="col-xs-2">
+					<h6>Connect:</h6>
+					<ul class="list-unstyled">
+						<li><a href="#">Facebook</a></li>
+						<li><a href="#">Twitter</a></li>
+						<li><a href="#">LinkedIn</a></li>
+						<li><a href="#">Bollenti Spiriti</a></li>
+					</ul>
+				</div>
 
 			</footer>
 
