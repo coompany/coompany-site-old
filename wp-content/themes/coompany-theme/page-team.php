d2286eeb3e831103c784e5b6f7fdb478b27f9901
Andrea Jemmett <andreajemmett@gmail.com>
2013-12-27 11:29:37 +0100

    Completed Team page

--- wp-content/themes/coompany-theme/page-team.php
+++ wp-content/themes/coompany-theme/page-team.php
@@ -12,6 +12,49 @@
 echo get_header();
 ?>
 
+			<section class="row">
+				<div class="col-md-8">
+					<?php $page_chi_siamo = get_post(119);		// page 119 is Chi siamo ?>
+					<h2><?php echo $page_chi_siamo->post_title; ?></h2>
+					<div class="content">
+						<?php echo apply_filters('the_content', $page_chi_siamo->post_content); ?>
+					</div>
+				</div>
+				<div class="col-md-3 col-md-offset-1" id="page-contatti">
+					<?php $page_contatti = get_post(148);		// page 148 is Contatti ?>
+					<h3><?php echo $page_contatti->post_title; ?></h3>
+					<div class="content">
+						<?php echo apply_filters('the_content', $page_contatti->post_content); ?>
+					</div>
+				</div>
+			</section>
 
+			<section id="section-team-members" class="row">
+				<div class="col-xs-12">
+					<?php
+					query_posts('post_type=team&posts_per_page=-1');
+					if(have_posts()) :
+						echo '<div class="row">';
+						$i = 0;
+						while(have_posts()) : the_post();
+							$image_url = wp_get_attachment_url( get_post_thumbnail_id(get_the_ID()) );
+							?>
+					<div class="col-sm-3 team-member<?php echo(($i%3!=0) ? (' col-sm-offset-1') : (null)); ?>">
+						<img class="img-circle" src="<?php echo $image_url; ?>" />
+						<h4><?php echo get_the_title(); ?><br /><small><?php echo get_field('role'); ?></small></h4>
+						<p class="content"><?php echo get_the_content(); ?></p>
+					</div>
+							<?php
+							$i++;
+						endwhile;
+						echo '</div>';
+					else :
+						?>
+					<h5>Nessun membro del team presente...</h5>
+						<?php
+					endif;
+					?>
+				</div>
+			</section>
 
 <?php echo get_footer(); ?>
