51c7bb1c7cf038c43979874672f6df7d2da4faa2
Andrea Jemmett <andreajemmett@gmail.com>
2014-06-11 16:28:15 +0200

    Bug fix

--- wp-content/themes/coompany-theme/navbar.php
+++ wp-content/themes/coompany-theme/navbar.php
@@ -10,7 +10,7 @@
 
 <nav class="navbar<?php echo((is_home()) ? (null) : (' navbar-default navbar-fixed-top')); ?>">
 	<div class="row">
-		<div class="navbar-header logo">
+		<div class="navbar-header<?php if(is_home()) { echo ' logo'; } ?>">
 			<a class="navbar-brand" href="<?php bloginfo('siteurl'); ?>">
 				<h1 class="sr-only"><?php bloginfo('name'); ?></h1>
 				<img src="<?php echo get_template_directory_uri(); ?>/images/coom-logo.png" />
