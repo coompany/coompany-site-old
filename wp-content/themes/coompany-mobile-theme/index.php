52c9240cea77dd9475d6c5d9b3cdb831dde3a400
Andrea Jemmett <andreajemmett@gmail.com>
2014-10-14 14:35:52 +0200

    Added the about section

--- wp-content/themes/coompany-mobile-theme/index.php
+++ wp-content/themes/coompany-mobile-theme/index.php
@@ -113,6 +113,70 @@ query_posts( 'post_type=features&prodotti=coo2plan&orderby=menu_order&order=ASC'
                     <?php endif; ?>
                 </section>
 
+
+
+                <section id="mobile-about-section" class="col-xs-12">
+
+                    <section id="about-section" class="row">
+                        <div class="col-xs-12">
+                            <?php $page_chi_siamo = get_post(119);		// page 119 is Chi siamo ?>
+                            <h2 style="margin-bottom: 30px"><?php echo $page_chi_siamo->post_title; ?></h2>
+                            <div class="content">
+                                <?php echo apply_filters('the_content', $page_chi_siamo->post_content); ?>
+                            </div>
+                        </div>
+                    </section>
+
+                    <hr />
+
+                    <section id="team-members" class="row">
+                        <div class="col-xs-12">
+                            <?php
+                            query_posts('post_type=team&posts_per_page=-1&order=asc');
+                            if(have_posts()) :
+                                while(have_posts()) : the_post();
+                                    ?>
+                                    <div class="row">
+                                        <div class="col-xs-12 team-member">
+                                            <img class="img-circle" src="<?php echo wp_get_attachment_url( get_post_thumbnail_id(get_the_ID()) ); ?>" />
+                                            <h4><?php echo get_the_title(); ?><br /><small><?php echo get_field('role'); ?></small></h4>
+                                            <p class="content"><?php echo get_the_content(); ?></p>
+                                        </div>
+                                    </div>
+                                <?php
+                                endwhile;
+                            else :
+                                ?>
+                                <h5>Nessun membro del team presente...</h5>
+                            <?php
+                            endif;
+                            ?>
+                        </div>
+                    </section>
+
+                    <section class="row" id="istituzionale">
+                        <div class="col-xs-12">
+                            <div class="row">
+                                <div class="col-xs-12">
+                                    <p>Vincitore del concorso Principi Attivi 2012</p>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-xs-12">
+                                    <section class="row">
+                                        <div class="col-xs-12">
+                                            <div class="logo-istituzionale">
+                                                <img src="<?php echo get_template_directory_uri(); ?>/library/images/vincitore_vert_1.jpg" />
+                                            </div>
+                                        </div>
+                                    </section>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+
+                </section>
+
             </div>
 
 <?php get_footer(); ?>
