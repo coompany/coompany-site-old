b22e956d74772278587ba94d45d0555f15d75ea8
Andrea Jemmett <andreajemmett@gmail.com>
2015-03-22 18:48:42 +0100

    better youtube iframe

--- wp-content/themes/coompany-mobile-theme/index.php
+++ wp-content/themes/coompany-mobile-theme/index.php
@@ -26,7 +26,7 @@ query_posts( 'post_type=features&prodotti=coo2plan&orderby=menu_order&order=ASC'
                             $parallax = get_field('parallax_img');
                             $parallax_css = 'style="background-image: url('.$parallax.');"';
 
-                            $video = get_field('video');
+                            $video = get_field('video') . '?autohide=1&fs=0&modestbranding=1&color=white';
                             $video_iframe = '<iframe type="text/html" width="640" height="360" src="'.$video.'" frameborder="0"></iframe>';
                             ?>
                             <?php if($index==0) : // this is the first feature ?>
